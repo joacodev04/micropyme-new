2dd6eb2dcb7b85f6127664737ba15fb9122ec940
Joaquin <joaquin04murillo@gmail.com>
2026-03-05 14:00:25 -0300

    dashboard

---
+++ js/procesos.js
@@ -0,0 +1,9 @@
+const procesos = [
+    {id: 1, nombre: "Corte"},
+    {id: 2, nombre: "Confeccion"},
+    {id: 3, nombre: "Bordado"},
+    {id: 4, nombre: "Estampado"},
+    {id: 5, nombre: "Finishsing"},
+];
+
+localStorage.setItem("procesos", JSON.stringify(procesos));
