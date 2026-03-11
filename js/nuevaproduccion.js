379b3ef2507b31d8b7a6705173f940ee9e39605f
Joaquin <joaquin04murillo@gmail.com>
2026-03-11 11:50:54 -0300

    Nueva produccion

--- js/nuevaproduccion.js
+++ js/nuevaproduccion.js
@@ -63,22 +63,13 @@ if(tablaAvios){
 
 }
 
-// //Activar producción
-// const botonguardar = document.getElementById("activar-produccion");
+//Activar producción
 
-// botonguardar.addEventListener("click", () => {
-//   const nombreProducto = document.querySelector(".input[placeholder='Nombre del Producto']").value;
-//   const 
-// });
+const KEY_ACTUAL = "produccionActual";
+const KEY_LISTA = "producciones";
 
-// function guardarProductos() {
-//   let producciones = JSON.parse(localStorage.getItem("guardarProducto")) || [];
+let producciones = JSON.parse(localStorage.getItem(KEY_ACTUAL)) || {};
 
-//   const producto = document.getElementById
-  
-// }
-
-let producciones = {};
 
 function guardarPaso1(){
   producciones.producto = document.querySelector("#nombre-producto").value;
@@ -109,7 +100,13 @@ function guardarPaso2(){
 }
 
 function guardarPaso3(){
-  producciones.tela = document.querySelector("#select-tela").value;
+  const select = document.querySelector("#select-tela");
+
+  producciones.telaNombre = select.options[select.selectedIndex]?.text || "";
+  producciones.consumoTela = document.getElementById("consumo-tela").value;
+  producciones.piezaCerrada = document.getElementById("kg-por-pieza").value;
+  producciones.precio = document.getElementById("precio/kg").value;
+
 }
 
 function guardarPaso4(){
@@ -163,8 +160,33 @@ function guardarPaso(step){
   if(step === 2) guardarPaso3();
   if(step === 3) guardarPaso4();
   if(step === 4) guardarPaso5();
-  console.log(producciones);
+  
+ localStorage.setItem(KEY_ACTUAL, JSON.stringify(producciones));
 }
+
+
+//Evitar borrar lo escrito en los inputs
+const form = document.getElementById("form-produccion");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+});
+
+document.querySelectorAll("input, select, textarea").forEach((el) => {
+  el.addEventListener("input", () => {
+    const data = JSON.parse(localStorage.getItem("form-produccion")) || {};
+    data[el.id] = el.value;
+    localStorage.setItem("form-produccion", JSON.stringify(data));
+  });
+});
+
+window.addEventListener("DOMContentLoaded", () => {
+  const data = JSON.parse(localStorage.getItem("form-produccion")) || {};
+  Object.keys(data).forEach((id) => {
+    const el = document.getElementById(id);
+    if (el) el.value = data[id];
+  });
+});
+
 
 //Click en pasos (Nueva Produccion)
 
@@ -205,4 +227,17 @@ document.querySelectorAll(".btnSiguiente").forEach(btn => {
 
   });
 
+});
+
+document.getElementById("activar-produccion").addEventListener("click", (e) => {
+  e.preventDefault();
+
+  const lista = JSON.parse(localStorage.getItem(KEY_LISTA)) || [];
+  const actual = JSON.parse(localStorage.getItem(KEY_ACTUAL)) || {};
+
+  lista.push({ ...actual, id: Date.now() });
+  localStorage.setItem(KEY_LISTA, JSON.stringify(lista));
+
+  localStorage.removeItem(KEY_ACTUAL);
+  producciones = {};
 });
